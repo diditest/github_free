7f1520faee9457d44703f81b50335612fd9095dd
diditest <didi.github.free@gmail.com>
2013-10-16 11:33:06 +0800

    测试项目

---
+++ hellworld/src/HelloWorld.java
@@ -0,0 +1,11 @@
+/**
+ * User: didiyy
+ * Date: 13-10-16
+ * Time: ионГ11:31
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+
+}
